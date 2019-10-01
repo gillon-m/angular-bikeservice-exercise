efa0a85ee46a4fa9f373097bd5665f87da2d9d8e
Gillon <Gillon.Manalastas@mastercard.com>
2019-10-01 11:38:10 +1000

    moved angular client and bike service inot the same directory

--- src/main/java/com/manalastas/exercises/angular/bikeRegistrationService/repository/BikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manalastas.exercises.angular.bikeRegistrationService.repository;
-
-import com.manalastas.exercises.angular.bikeRegistrationService.model.Bike;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BikeRepository extends CrudRepository<Bike, Long> {
-}
